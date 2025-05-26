c21899e8ded254b9439bc47057647b4df437a6ce
Ayushi Singh <singhayushi9369@gmail.com>
2025-05-26 23:53:50 +0530

    DestructuringArray

--- JS Project/assets/scripts/app.js
+++ JS Project/assets/scripts/app.js
@@ -1,5 +1,7 @@
 // //import { apikey } from "./util.js";
 
+const { use } = require("react");
+
 // //import apikey from "./util.js";
 
 //  // import {apikey, abc} from "./util.js";
@@ -66,21 +68,35 @@
 // // since the function belongs to the object it can access other properties of that object 
 // //to access other methods or fields u can use this keyword 
 
-const hobbies = ["sports", "singing", "coding"];
-console.log(hobbies[1]); 
+// const hobbies = ["sports", "singing", "coding"];
+// console.log(hobbies[1]); 
 
-hobbies.push("Working");
-console.log(hobbies);
+// hobbies.push("Working");
+// console.log(hobbies);
 
 // const index = hobbies.findIndex ((item) => {
 //     return item === "sports";
 
-const index = hobbies.findIndex ((item) => item === "sports");
-console.log(index);
+// const index = hobbies.findIndex ((item) => item === "sports");
+// console.log(index);
 
-const editedHobbies = hobbies.map((item) => ({text:item}));
-console.log(editedHobbies);
+// const editedHobbies = hobbies.map((item) => ({text:item}));
+// console.log(editedHobbies);
 
 // items can not only be converted into strings but also into JS objects.
 // object created with curly braces and wrapped around () and then key value-pair is given. 
 // it returns text:sports 
+
+const [firstname,lastname] = ["Ayushi", "Singh"];
+
+// const firstName = userName[0];
+// const lastName = userName[1];
+
+console.log(firstname);
+console.log(lastname);
+
+// instead of using username[indexno] now can be accessed using firstname and lastname.
+// but also a better way to get the value is to deconstruct the array.
+// the 2 variables can be created in 1 single step by adding [] on left side.
+// on right side they create new array on left side they destructure that array by pulling out the values.
+// firstname pulls Ayushi ,lastname pulls singh
