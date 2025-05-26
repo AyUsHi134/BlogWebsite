236d6f3780083936ad3d98589bd249af62c579de
Ayushi Singh <singhayushi9369@gmail.com>
2025-05-26 13:09:49 +0530

    Objects

--- JS Project/assets/scripts/app.js
+++ JS Project/assets/scripts/app.js
@@ -1,49 +1,68 @@
-//import { apikey } from "./util.js";
+// //import { apikey } from "./util.js";
 
-//import apikey from "./util.js";
+// //import apikey from "./util.js";
 
- // import {apikey, abc} from "./util.js";
+//  // import {apikey, abc} from "./util.js";
 
- //import * as util from "./util.js";
-//console.log(util.apikey); 
+//  //import * as util from "./util.js";
+// //console.log(util.apikey); 
 
-//console.log("Hello world"); 
-// values can be created directly 
+// //console.log("Hello world"); 
+// // values can be created directly 
 
-//let userMessage = "Hello World";
-//values are stored in variables 
-//variables are data containers which carries value and u can name them as u like
+// //let userMessage = "Hello World";
+// //values are stored in variables 
+// //variables are data containers which carries value and u can name them as u like
 
-//console.log(userMessage);
-// these variables can be then used as identifiers to access the value whenever u want.
+// //console.log(userMessage);
+// // these variables can be then used as identifiers to access the value whenever u want.
 
-//const username = "hello world";
+// //const username = "hello world";
 
-//another way to create variables is const
-// const value cannot be reassigned but if using let it can be done.
+// //another way to create variables is const
+// // const value cannot be reassigned but if using let it can be done.
 
-function greet() {
-    console.log("Hello world");
-}
-//code inside the function does not run directly but instead whne function is called.
+// function greet() {
+//     console.log("Hello world");
+// }
+// //code inside the function does not run directly but instead whne function is called.
 
-greet(); // calling the function 
-// function can be called as many times as u want 
+// greet(); // calling the function 
+// // function can be called as many times as u want 
 
-function greet(username, Message = "hello") {
-   // console.log(username);
-    //console.log(Message);
+// function greet(username, Message = "hello") {
+//    // console.log(username);
+//     //console.log(Message);
 
-    return "hi, i am " + username + Message;
-}
+//     return "hi, i am " + username + Message;
+// }
 
-const greetings = greet("Ayush");
-console.log(greetings);
+// const greetings = greet("Ayush");
+// console.log(greetings);
 
-export default (username, Message) => {
-console.log("hello");
-return username + Message;
-}
+// export default (username, Message) => {
+// console.log("hello");
+// return username + Message;
+// }
 // another way of opening function is using arrow method
 // everything can be done in functions like this
 // functions which do not have a name are created are this
+
+
+// const user = {
+//     name :ayushi,
+//     age : 24,
+//     userGreet(){
+//         console.log("Hello");
+//         console.log(this.age);
+//     }
+// };
+
+// console.log(user.name)
+// user.userGreet(); // calling method the same way just using . to access the function as it is the object 
+// // and calling the function as we normally do 
+
+// // objects can also store functions or called methods
+// // since the function belongs to the object it can access other properties of that object 
+// //to access other methods or fields u can use this keyword 
+
