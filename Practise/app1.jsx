fa98f934eebd1a9a5d9051f04f6e557d78719054
Ayushi Singh <singhayushi9369@gmail.com>
2025-06-02 21:33:13 +0530

    Practise

---
+++ Practise/app1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const userData = {
+  firstName: 'Ayushi', 
+  lastName: 'Singh', 
+  title: 'student', 
+};
+
+
+export function User() {
+  return (
+    <div id="user" data-testid="user">
+      <h2>
+        {userData.firstName} {userData.lastName}
+      </h2>
+      <p>{userData.title}</p>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div id="app">
+      <h1>Time to Practice</h1>
+      <p>Welcome on board of this course! You got this 💪</p>
+      <User />
+    </div>
+  );
+}
+
+export default App;
