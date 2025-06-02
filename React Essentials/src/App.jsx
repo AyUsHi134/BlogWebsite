a203cdadcfbdb3e161c7982952c8347dc8b88cb0
Ayushi Singh <singhayushi9369@gmail.com>
2025-06-03 02:29:28 +0530

    ReactProps

--- React Essentials/src/App.jsx
+++ React Essentials/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg  from './assets/components.png';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 
@@ -22,15 +23,49 @@ function Header() {
   );
 }
 
+
+
+function CoreConcepts(props) {
+  return(
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       {/*<Header></Header>*/ }
       <Header /> 
       <main>
-        <h2>Time to get started!</h2>
+        <section id= "core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts 
+          title="Components"
+          description="The core UI building block"
+          img = {componentsImg}
+          />
+          <CoreConcepts
+          title="Components"
+          description="The core UI building block"
+          img = {componentsImg}
+          />
+          <CoreConcepts 
+          title="Components"
+          description="The core UI building block"
+          img = {componentsImg}
+          />
+          <CoreConcepts title="Components"
+          description="The core UI building block"
+          img = {componentsImg}
+          />
+        </ul></section>
       </main>
-    </div>
+    </div>  
   );
 }
 
