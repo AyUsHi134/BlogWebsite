5771755027bf9ab5070e28e5af2f424077f19d4d
Ayushi Singh <singhayushi9369@gmail.com>
2025-06-03 15:45:45 +0530

    Practise

---
+++ Practise/app2.jsx
@@ -0,0 +1,29 @@
+export function CourseGoal(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>Time to Practice</h1>
+      <p>One course, many goals! 🎯</p>
+      <ul>
+        {/* OUTPUT AT LEAST TWO CourseGoal components here */}
+        {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+        <CourseGoal 
+        title="Learn React"
+        description="In-depth"
+        />
+        <CourseGoal 
+        title="Learn Python"
+        description="something"
+        />
+      </ul>
+      </div>
+  );
+}
